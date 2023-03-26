f665639eca6cef1ab462f474894bb6b23aa5160f
Nishanth Kiran <nishanthkiran2@gmail.com>
2023-03-26 23:51:52 +0530

    some more weather modifications

--- startmyday.sh
+++ startmyday.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
 
-# greet, weather
+# initializing variables
 name=$(whoami)
 ip=$(curl -s https://ipinfo.io/ip)
 city=$(curl -s http://ip-api.com/json/$ip | jq '.["city"]')
 status=$(curl -s http://ip-api.com/json/$ip | jq '.["status"]')
 weather=$(curl -s wttr.in/$city?format=3)
 
+# greetings
 greet() {
     echo "Hello $name"
     sleep 2
     echo "Hope you'll have a wonderful day ahead"
 }
 
+# weather
 weather() {
-    echo "Want to know weather based on your ip location? (y/n)"
+    echo "How do you want your weather update. Based on your:"
+    echo "1. IP Location"
+    echo "2. Default Location"
+    echo "3. Manual location"
     read location
 
-    if [ $location == 'y' || $location == 'Y' ]; then
-        if [ $status == '"success"' ]; then
-            echo "Today's weather in $weather"
-        else 
-            echo "Didn't find the city you live in sorry :(. Would be interested in knowing about the weather today?(y/n)"
-            
-            if [ $response == 'y' || $response == 'Y' ]; then
-                echo "Please mention your city"
-                read city
-            else
-                echo "Okay I hope the weather is good :)"
+    case $location in
+        1)
+            if [ $status == '"success"' ]; then
+                echo "Today's weather in $weather"
+                ;;
+            else 
+                echo "Didn't find the city you live in sorry :(."
+                ;;
             fi
-        fi
-    else
-        read 
-    fi
+
+        2)
+            echo "Please provide your default city"
+            read dCity
+
+            # insert the city inside bashrc. If the city is already present then don't prompt for city. If the user wants to change the default location then that question should be asked in the echo above
+
+            weather=$(curl -s wttr.in/$city?format=3)
+            echo "Today's weather in $weather"
+            ;;
+
+        3)
+            echo "Please provide your default city"
+            read city
+
+            weather=$(curl -s wttr.in/$city?format=3)
+            echo "Today's weather in $weather"
+            ;;
 }
 
 flow() {
     greet
     sleep 2
     
-    weather
+    echo "Want some weather updates? (y/n)"
+    read response
+
+    if [ $response == 'y' || $response == 'Y' ]; then
+        weather
+    fi
 }
 
 flow
